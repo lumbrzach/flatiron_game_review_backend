639e3eff9106dc8249cc777bb54ca366ad7c1591
github username <github email address>
2019-07-31 10:55:59 -0400

    updates

--- app/controllers/api/v1/platforms_controller.rb
+++ app/controllers/api/v1/platforms_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::PlatformsController < ApplicationController
         platforms = Platform.all
         render json: platforms
     end
+
+    def show
+        platform = Platform.find(params[:id])
+        render json: platform
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,8 @@ class Game < ApplicationRecord
     has_many :ratings, dependent: :destroy
     has_many :game_platforms, dependent: :destroy
     has_many :platforms, through: :game_platforms
+
+    # def as_json(options={})
+    #     { :comments => self.comments, :likes => self.likes, :ratings => self.ratings}
+    # end
 end
